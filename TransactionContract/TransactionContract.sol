fe2b7319c4d2c2d72e56b09bd8944c20f9d12e71
Erkan <erkan.erolov321@Gmail.com>
2024-10-06 22:41:16 +0300

    reduce of gas

--- TransactionContract/TransactionContract.sol
+++ TransactionContract/TransactionContract.sol
@@ -5,28 +5,27 @@ pragma solidity 0.8.26;
 
 import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
 
+error Unathorized();
+
+error NotOwner(address notOwnerAddress);
 
 contract FundMe{
 
-    uint256 public minimumUsd = 5e18;
+    uint256 public constant MINIMUM_USD = 5e18;     //Constant saves Gas since this Value will never change
 
-   
-    address transactionAddress;
-        
-  
     address[] public funders;
 
     mapping(address funder => uint256 amountFunded) public addressToAmountFunded;
 
-    address public owner;
+    address public immutable i_owner;
 
     constructor() {
-        owner = msg.sender;
+        i_owner = msg.sender;
     }
 
     function fund() public payable{
 
-        require(getConversionRate(msg.value) >= minimumUsd, "Didnt send enough ETH");
+        require(getConversionRate(msg.value) >= MINIMUM_USD, "Didnt send enough ETH");
         funders.push(msg.sender);
         addressToAmountFunded[msg.sender] +=  msg.value;
     }
@@ -36,7 +35,6 @@ contract FundMe{
         for(uint256 getAddressIndex = 0; getAddressIndex < funders.length; getAddressIndex++){
             address getTempAddress = funders[getAddressIndex];
             addressToAmountFunded[getTempAddress] = 0;
-            
         }
 
         funders = new address[](0);
@@ -56,6 +54,7 @@ contract FundMe{
         uint256 ethPrice = getPrice();
         uint256 ethAmountInUsd = (ethPrice * ethAmount) / 1e18;
         return ethAmountInUsd;
+        
     }
 
     function getVersion() public view returns(uint256){
@@ -63,7 +62,10 @@ contract FundMe{
     }
 
     modifier onlyOwner() {  //ADDS SECURITY  GIVES ACCESS ONLY TO SPECIFIC OWNERS ADDRESS PERMISSION TO WITHDRAW FROM CONTRACT
-        require(msg.sender == owner, "Must be owner address !");
+        // require(msg.sender == i_owner, "Must be owner address !");
+        if (msg.sender != i_owner) {
+            revert NotOwner({notOwnerAddress: msg.sender});
+        }
         _;
     }
 
